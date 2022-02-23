7236518d8607fbaf820c3501e03d462b11d2d97e
zyq1569 <zyq1569@126.com>
2022-02-23 17:07:32 +0800

    fine-tune

--- kxmlgui/tests/kxmlGuiMain/main.cpp
+++ kxmlgui/tests/kxmlGuiMain/main.cpp
@@ -1,10 +1,11 @@
 #include "mainwindow.h"
-
+#include <klocalizedstring.h>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    KLocalizedString::setApplicationDomain("calligrawords");
     MainWindow w;
     w.show();
     return a.exec();
--- kxmlgui/tests/kxmlGuiMain/mainwindow.cpp
+++ kxmlgui/tests/kxmlGuiMain/mainwindow.cpp
@@ -1,5 +1,10 @@
 #include "mainwindow.h"
+#include "kswitchlanguagedialog.h"
 
+//#include <QAction>
+//#include <QTextEdit>
+//#include <QApplication>
+using namespace KDEPrivate;
 MainWindow::MainWindow(QWidget *parent)
 //    : QMainWindow(parent)
     : KXmlGuiWindow(parent)
@@ -12,10 +17,64 @@ MainWindow::MainWindow(QWidget *parent)
                     QStringLiteral("kxmlGuiMain/kxmlGuiMain.rc"));
 
     //setCentralWidget(new QTextEdit(this));
+    setupActions();
+}
+
+void MainWindow::slotTest()
+{
+    KMessageBox::information(nullptr, QStringLiteral("--Test--"), QStringLiteral("caption"));
+
+    static KSwitchLanguageDialog * dialog =  new KSwitchLanguageDialog(this);
+    if (dialog)
+    {
+        dialog->show();
+    }
+}
+
+void MainWindow::slotCreate()
+{
+    //setStandardToolBarMenuEnabled(true);
+    setupGUI(ToolBar|Keys);
+    //setupGUI(ToolBar | Keys |  Save );
+    createGUI(xmlFile());
+
+//    if (autoSaveConfigGroup().isValid())
+//    {
+//        applyMainWindowSettings(autoSaveConfigGroup());
+//    }
+}
+
+void MainWindow::setupActions()
+{
+    QAction *testAction = new QAction(this);
+    testAction->setText(QStringLiteral("TestWindow"));
+    //testAction->setIcon(QIcon::fromTheme(QStringLiteral("kde")));
+    actionCollection()->addAction(QStringLiteral("test"), testAction);
+    connect(testAction, SIGNAL(triggered(bool)), this, SLOT(slotTest()));
+
+    KStandardAction::quit(qApp, SLOT(quit()), actionCollection());
+
+    setAutoSaveSettings();
+
+//    // BUG: if the GUI is created after an amount of time (so settings have been saved), then toolbars
+//    //      are shown misplaced. KMainWindow uses a 500 ms timer to save window settings.
+#ifdef REPRODUCE_TOOLBAR_BUG
+    QTimer::singleShot(1000, this, SLOT(slotCreate())); // more than 500 ms so the main window has saved settings.
+    // We can think of this case on natural applications when they
+    // load plugins and change parts. It can take 1 second perfectly.
+#else
+    QTimer::singleShot(0, this, SLOT(slotCreate()));
+#endif
 }
 
 MainWindow::~MainWindow()
 {
 
 }
+
+
+
+
+
+
 
